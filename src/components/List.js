cc6eba6bdaef55ef7710729d5b5cc0e00d775240
Grigory Moshkevich <ilcostuff@gmail.com>
2017-10-31 13:15:41 +0300

    bunch of c..

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Task from './Task';
 
 var todoCounter = 3;
+var tasksFilter = 'All';
 
 class List extends Component {
   constructor(props) {
@@ -9,55 +10,79 @@ class List extends Component {
     this.state = {list: [
       {
         id: 1,
-        text: 'water plants'
+        text: 'water plants',
+        status: 'active'
       },
       {
         id: 2,
-        text: 'feed mammals'
+        text: 'feed mammals',
+        status: 'active'
       }
     ]};
   }
 
-  addTask() {
+  handleAddTask() {
     let newList;
     newList = this.state.list.concat({
       id: todoCounter++,
-      text: ''
+      text: '',
+      status: 'active'
     })
-    // console.log(newList);
     this.setState({list: newList});
   }
 
-  editTask(taskIdToEdit, text) {
-    let newList;
-    newList = this.state.list;
-    newList.forEach((task) => {
-      if (task.id === taskIdToEdit) {
-        task.text = text;
-      }
-    })
-    // console.log(this.state);
-    // console.log(newList);
+  handleCompleteTask(taskId) {
+    let newList = [...this.state.list];
+    let taskIndex = newList.findIndex(task => task.id === taskId);
+    newList[taskIndex] = {
+        id: newList[taskIndex].id,
+        text: newList[taskIndex].text,
+        status: newList[taskIndex].status === 'active' ? 'completed' : 'active'
+    };
+    console.log(newList);
+    console.log(this.state.list);
     this.setState({list: newList});
   }
 
-  deleteTask(taskIdToDelete) {
-    let newList;
-    newList = this.state.list.filter(task => task.id !== taskIdToDelete);
+  handleEditTask(taskId, newText) {
+    let newList = [...this.state.list];
+    let taskIndex = newList.findIndex(task => task.id === taskId);
+    newList[taskIndex] = {
+        id: newList[taskIndex].id,
+        text: newText,
+        status: newList[taskIndex].status
+    };
+    // console.log(newList);
+    // console.log(this.state.list);
     this.setState({list: newList});
+  }
+
+  handleDeleteTask(taskId) {
+    let newList;
+    newList = this.state.list.filter(task => task.id !== taskId);
+    this.setState({list: newList});
+  }
+
+  changeFilter(val) {
+    tasksFilter = val;
+    this.forceUpdate();
   }
 
   render() {
     let tasks;
     tasks = this.state.list.map(task => {
-      return (
-        <Task 
-          key={task.id} 
-          onEdit={this.editTask.bind(this)} 
-          onDelete={this.deleteTask.bind(this)} 
-          task={task} 
-        />
-      );
+      console.log(tasksFilter);
+      if (task.status === tasksFilter || tasksFilter === 'All') {
+        return (
+          <Task 
+            key={task.id} 
+            onCompleteToggle={this.handleCompleteTask.bind(this)} 
+            onEdit={this.handleEditTask.bind(this)} 
+            onDelete={this.handleDeleteTask.bind(this)} 
+            task={task} 
+          />
+        );
+      } else {return null}
     })
     
     return (
@@ -65,10 +90,27 @@ class List extends Component {
         <div className="row justify-content-center">
           <div className="List col-12 col-md-9 col-lg-7 col-xl-6">
             <h4 className="text-center">Todo list</h4>
+            <div className="btn-group d-flex btn-group-lg" role="group" aria-label="Basic example">
+              <button type="button" 
+                className={"btn btn-info w-100" + (tasksFilter === 'All' ? ' active' : '')} 
+                onClick={this.changeFilter.bind(this, 'All')}>
+                All
+              </button>
+              <button type="button" 
+                className={"btn btn-info w-100" + (tasksFilter === 'active' ? ' active' : '')}
+                onClick={this.changeFilter.bind(this, 'active')}>
+                Active
+              </button>
+              <button type="button" 
+                className={"btn btn-info w-100" + (tasksFilter === 'completed' ? ' active' : '')} 
+                onClick={this.changeFilter.bind(this, 'completed')}>
+                Completed
+              </button>
+            </div>
             <ul className="list-group">
               {tasks}
             </ul>
-            <button type="button" className="btn btn-block btn-primary btn-lg" onClick={this.addTask.bind(this)}>
+            <button type="button" className="btn btn-block btn-primary btn-lg" onClick={this.handleAddTask.bind(this)}>
               <i className="fa fa-plus" aria-hidden="true"></i>
             </button>
           </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -6,34 +6,36 @@ class Task extends Component {
     this.state = {completed: false};
   }
 
-  completeTaskToggle() {
-    // this.props.onComplete(taskIdToComplete)
+  completeTaskToggle(taskId) {
+    // this.props.onComplete(taskId)
     this.setState({completed: !this.state.completed});
+    this.props.onCompleteToggle(taskId);
   }
 
-  deleteTask(taskIdToDelete) {
-    this.props.onDelete(taskIdToDelete)
+  deleteTask(taskId) {
+    this.props.onDelete(taskId);
   }
 
-  editTask(taskIdToEdit, event) {
-    this.props.onEdit(taskIdToEdit, event.target.value)
+  editTask(taskId, event) {
+    this.props.onEdit(taskId, event.target.value);
   }
   
   render() { 
+    let val = this.state.completed ? (this.props.task.text ? this.props.task.text : 'new task') : this.props.task.text;
     return (
       <li className={"Task list-group-item" + (this.state.completed ? ' list-group-item-success' : '')}>
         <div className="input-group">
           <input 
             type="text" 
-            className="form-control" 
-            value={this.props.task.text} 
+            className="form-control text-truncate" 
+            value={val}
             placeholder="new task"
             onChange={this.editTask.bind(this, this.props.task.id)}
             disabled={this.state.completed}
           />
           <span className="input-group-btn">
             <button type="button" 
-              className={"btn btn-success" + (this.state.completed ? ' darkGreenButton' : '')} 
+              className={"btn btn-success" + (this.props.task.status === 'completed' ? ' completedTaskCompleteButton' : '')} 
               onClick={this.completeTaskToggle.bind(this, this.props.task.id)}>
               <i className="fa fa-check" aria-hidden="true"></i>
             </button>
